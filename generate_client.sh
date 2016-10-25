60e8d72fd83d86f372539cc23254b7e31bac1548
Rafael Roman <panthro.rafael@gmail.com>
2016-10-25 13:55:26 +0200

    removing old version of client, adding package and artifact

--- generate_client.sh
+++ generate_client.sh
@@ -4,5 +4,12 @@ then
   echo "Example: ./generate_client.sh java"
   exit 1
 fi
+rm -rf $1
 
-docker run -v `pwd`:/app -w /app --rm jimschubert/swagger-codegen-cli generate -i swagger.yaml -l $1 -o $1
+docker run -v `pwd`:/app -w /app --rm jimschubert/swagger-codegen-cli generate \
+-i swagger.yaml \
+-l $1 \
+-o $1 \
+--api-package com.github.panthro.hockeyapp \
+--group-id com.github.panthro.hockeyapp \
+--artifact-id hockeyapp-api
